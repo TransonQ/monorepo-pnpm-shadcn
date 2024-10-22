9445d9ac88569adadd117fc01a80d490713ddaf6
TransonQ <quanscheng@qq.com>
2024-10-22 21:43:01 +0800

    feat: 命令行 --template 空格隔开模板名字

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 ## scripts
 
-根据模板创建新项目
-
 ```bash
-pnpm run new-app --template=vite-app
+# 根据模板 vite-app 在 apps/* 工作去创建一个新项目
+pnpm run new-app --template vite-app
 ```
--- scripts/copy-template.cjs
+++ scripts/copy-template.cjs
@@ -2,16 +2,22 @@
 const fs = require("fs-extra")
 const path = require("path")
 
-// 从命令行参数中获取模板名称
+// 获取命令行参数数组
 const args = process.argv.slice(2)
-const templateArg = args.find((arg) => arg.startsWith("--template="))
-if (!templateArg) {
+
+// 查找 --template 参数的位置
+const templateIndex = args.indexOf("--template")
+
+// 检查是否提供了 --template 参数和模板名称
+if (templateIndex === -1 || !args[templateIndex + 1]) {
   console.error(
-    "Error: Please provide a template name using --template=<template-name>"
+    "Error: Please provide a template name using --template <template-name>"
   )
   process.exit(1)
 }
-const templateName = templateArg.split("=")[1]
+
+// 获取模板名称
+const templateName = args[templateIndex + 1]
 
 // 定义模板目录和目标目录
 const templatesDir = path.join(__dirname, "../templates")
