d5e9f5ee98ae51675a9b275d4ec4d70a6c7953cd
TransonQ <quanscheng@qq.com>
2024-10-23 15:49:05 +0800

    feat(templates): vite-app

--- templates/vite-app/src/App.tsx
+++ templates/vite-app/src/App.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@ui/shadcn/button"
+import { useState } from "react"
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <>
-      <Button>Button</Button>
-      <div className="text-red-500">Hello World</div>
-      <div>Tailwind css</div>
-    </>
+    <div className="p-16 flex flex-col items-center gap-8">
+      <Button onClick={() => setCount((count) => count + 1)}>
+        Button clicked + {count}
+      </Button>
+      <div className="text-sky-600 font-bold">{"text-sky-600 font-bold"}</div>
+    </div>
   )
 }
 
