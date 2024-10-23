47a65d9c2f7fe2d9126e3f0ca8bb94b6501a985f
TransonQ <quanscheng@qq.com>
2024-10-23 16:29:19 +0800

    feat: SiderbarDemo07Page

--- templates/admin-kit/src/App.tsx
+++ templates/admin-kit/src/App.tsx
@@ -1,17 +1,7 @@
-import { Button } from "@ui/shadcn/button"
-import { useState } from "react"
+import { SiderbarDemo07Page } from "./layout/page"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="p-16 flex flex-col items-center gap-8">
-      <Button onClick={() => setCount((count) => count + 1)}>
-        Button clicked + {count}
-      </Button>
-      <div className="text-sky-600 font-bold">{"text-sky-600 font-bold"}</div>
-    </div>
-  )
+  return <SiderbarDemo07Page />
 }
 
 export default App
